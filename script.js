e9666723af0987f766d48a92ba1a75b0c6b5b17f
bofletcher <hfletche.hf@gmail.com>
2020-11-18 10:15:40 -0500

    testing events

--- script.js
+++ script.js
@@ -2,6 +2,20 @@ let guess = 500;
 let max = 1000;
 let min = 1;
 
+let upBtn = document.getElementById("upButton");
+upBtn.addEventListener("click", guessHigher);
+
+let dwnBtn = document.getElementById("downButton");
+dwnBtn.addEventListener("click", guessLower);
+
+function guessHigher() {
+  console.log("HIGHER");
+}
+
+function guessLower() {
+  console.log("LOWER")
+}
+
 console.log(`Guess a number between ${min} and ${max}`);
 console.log(`Is you number higer or lower than ${guess}?`);
 console.log('Press the Up Arrow for higher and the Down Arrow for lower. Press enter is the guess is your number.')
