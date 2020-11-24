e6a8c1a9a25a3f405423c42b0c0be613d51ca21f
bofletcher <hfletche.hf@gmail.com>
2020-11-24 10:17:01 -0500

    completed simple number wizard game for practice.

--- script.js
+++ script.js
@@ -8,28 +8,33 @@ upBtn.addEventListener("click", guessHigher);
 let dwnBtn = document.getElementById("downButton");
 dwnBtn.addEventListener("click", guessLower);
 
+let correctBtn = document.getElementById("correctBtn").addEventListener("click" , correct)
+
 function guessHigher() {
   console.log("HIGHER");
+  min = guess;
+  guess = Math.round((max + min) / 2) 
+  guessTxt.innerHTML = `Is you number higer or lower than ${guess}?`;
+  console.log(guess)
 }
 
 function guessLower() {
-  console.log("LOWER")
+  max = guess;
+  guess = Math.round((max + min) / 2) 
+  guessTxt.innerHTML = `Is you number higer or lower than ${guess}?`;
+  console.log(guess)
 }
 
-console.log(`Guess a number between ${min} and ${max}`);
-console.log(`Is you number higer or lower than ${guess}?`);
-console.log('Press the Up Arrow for higher and the Down Arrow for lower. Press enter is the guess is your number.')
+function correct() {
+  guessTxt.innerHTML = `Your number is ${guess}`
+}
+
 max = 1000 + 1
 
-document.addEventListener("keydown", function(e) {
-  console.log(e.keyCode)
-  if(e.keyCode == 13){
-    console.log(`Your number is ${guess}`)
-  } else if (e.keyCode == 38) {
-    guess = Math.round((max + guess) / 2)
-    console.log(`Is your number ${guess}`)
-  } else if (e.keyCode == 40) {
-    guess = Math.round((min + guess) / 2);
-    console.log(`Is your number ${guess}`)
-  }
-})
+
+
+let instructionTxt = document.getElementById("instructionsTxt");
+instructionTxt.innerHTML = `Guess a number between ${min} and ${max}`;
+
+let guessTxt = document.getElementById("guessTxt");
+guessTxt.innerHTML = `Is you number higer or lower than ${guess}?`;
